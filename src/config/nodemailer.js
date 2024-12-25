0964d915d6081ace2e9f023b184b4e671289b240
Dennis <dennis.diaz@epn.edu.ec>
2024-12-25 12:28:33 -0500

    Modelo Clientes

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: process.env.HOST_MAILTRAP,
   port: process.env.PORT_MAILTRAP,
+  secure:true,
   auth: {
       user: process.env.USER_MAILTRAP,
       pass: process.env.PASS_MAILTRAP,
--- src/routers/RouterProveedor.js
+++ src/routers/RouterProveedor.js
@@ -9,14 +9,14 @@ import { RecuperarContrasenia,
 
 const routeProveedor = Router()
 
-routeProveedor.post('/registroP',registroProve)
-routeProveedor.get('/confirmarP/:token',confirmarEmail)
-routeProveedor.post('/loginP', loginProve)
-routeProveedor.post('/actualizar-perfilP', ActualizarPerfilProveedor)
-routeProveedor.post('/actualizar-contraseniaP', ActualizarContraseniaProve)
+routeProveedor.post('/registroProveedor',registroProve)
+routeProveedor.get('/confirmarProveedor/:token',confirmarEmail)
+routeProveedor.post('/loginProveedor', loginProve)
+routeProveedor.post('/actualizar-perfilProveedor', ActualizarPerfilProveedor)
+routeProveedor.post('/actualizar-contraseniaProveedor', ActualizarContraseniaProve)
 
-routeProveedor.post('/recuperar-contrasenia', RecuperarContrasenia)
-routeProveedor.post('/restablecer-contrasenia/:token', ConfirmarRecuperarContrasenia)
+routeProveedor.post('/recuperar-contrasenia-prov', RecuperarContrasenia)
+routeProveedor.post('/restablecer-contrasenia-prov/:token', ConfirmarRecuperarContrasenia)
 
 
 export default routeProveedor
